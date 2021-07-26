c3321fab693581ee45837764cc44be2ef1e4bff7
BJ Viehl <william.viehl@3sharecorp.com>
2021-07-26 16:27:38 -0600

    Fixing ignores

--- ui.frontend/.eslintrc.js
+++ ui.frontend/.eslintrc.js
@@ -2,10 +2,12 @@ module.exports =  {
     parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
     extends:  [
       'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+      'airbnb-typescript/base'
     ],
     parserOptions:  {
       ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
       sourceType:  'module',  // Allows for the use of imports
+      project: './tsconfig.json'
     },
     rules:  {
         "curly": 1,
