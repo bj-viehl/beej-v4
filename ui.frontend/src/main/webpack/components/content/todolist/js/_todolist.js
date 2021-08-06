720412571eb9c66143411d47d607feab92a56082
BJ Viehl <viehl.design@gmail.com>
2021-08-06 15:44:02 -0600

    todo list updates and global styles

--- ui.frontend/src/main/webpack/components/content/todolist/js/_todolist.js
+++ ui.frontend/src/main/webpack/components/content/todolist/js/_todolist.js
@@ -25,7 +25,7 @@ function getTasks() {
     // Add class to link
     link.className = 'beej-todo-list__list-item-delete';
     // Add icon
-    link.innerHTML = '<i class="fa fa-remove"></i>';
+    link.innerHTML = '<i class="fa fa-times"></i>';
     // Append link to li
     li.appendChild(link);
     // Append li to ul
@@ -63,7 +63,7 @@ function addTask(e) {
   // Add class to link
   link.className = 'beej-todo-list__list-item-delete';
   // Add icon
-  link.innerHTML = '<i class="fa fa-remove"></i>';
+  link.innerHTML = '<i class="fa fa-times"></i>';
   // Append link to li
   li.appendChild(link);
   // Append li to ul
@@ -112,17 +112,27 @@ function clearTasks() {
 
 function filterTasks(e) {
   const text = e.target.value.toLowerCase();
+  
 
   document.querySelectorAll('.beej-todo-list__list-item').forEach((task) => {
     const item = task.firstChild.textContent;
     const taskI = task;
 
     if (item.toLowerCase().indexOf(text) !== -1) {
-      taskI.style.display = 'block';
+      taskI.style.display = 'flex';
     } else {
       taskI.style.display = 'none';
     }
   });
+
+  // const list = document.querySelector('.beej-todo-list__list');
+  // const noResults = document.querySelector('.no-results');
+
+  // if (list.children(':visible').length === 0) {
+  //   noResults.style.display = "block";
+  // } else {
+  //   noResults.style.display = "none";
+  // }
 }
 
 function loadEventListeners() {
