dfc110f901d07e2f7f05bd20d796645da2d3ad2a
BJ Viehl <viehl.design@gmail.com>
2021-08-23 16:48:14 -0600

    Making header udpates and including ts

---
+++ ui.frontend/src/main/webpack/components/structure/header/ts/header.ts
@@ -0,0 +1,42 @@
+class Header {
+  private static readonly HEADER = '.beej-header';
+  private static readonly HEADER_MENU = '.beej-header__menu';
+  private static readonly MENU_BUTTON = '.beej-header__menu-button';
+  private static readonly MENU_ICON = '.beej-header__menu-button-icon--menu';
+  private static readonly CLOSE_ICON = '.beej-header__menu-button-icon--close';
+
+  private header: HTMLElement;
+
+    constructor() {
+      this.init();
+    }
+
+    init = () => {
+      this.header = document.querySelector(Header.HEADER) as HTMLElement;
+      const button = document.querySelector(Header.MENU_BUTTON) as HTMLElement;
+
+      if (button) {
+        button.addEventListener("click", this.toggleMenu);
+      }
+    }
+
+    private toggleMenu = () => {
+      const menu = this.header.querySelector(Header.HEADER_MENU) as HTMLElement;
+      const menuIcon = this.header.querySelector(Header.MENU_ICON)as HTMLElement;
+      const closeIcon = this.header.querySelector(Header.CLOSE_ICON)as HTMLElement;
+
+      if (menu.classList.contains('beej-header__menu--active')) {
+          menu.classList.remove('beej-header__menu--active');
+          menuIcon.style.display = 'block';
+          closeIcon.style.display = 'none';
+      } else {
+          setTimeout(() => {
+              menu.classList.add('beej-header__menu--active');
+              menuIcon.style.display = 'none';
+              closeIcon.style.display = 'block';
+          }, 50);
+      }
+  }
+}
+
+const header = new Header();
