2e4ddf29b016b62b230f34858011859c0396aa0a
BJ Viehl <viehl.design@gmail.com>
2021-08-24 14:13:26 -0600

    adding back to top button to footer component and adding styles and js

---
+++ ui.frontend/src/main/webpack/components/structure/backtotop/ts/_backtotop.ts
@@ -0,0 +1,32 @@
+class BackToTop {
+  constructor() {
+    this.init();
+  }
+  private init() {
+    const scrollTopButton = document.querySelector('.beej-back-to-top-button') as HTMLElement;
+
+    scrollTopButton.addEventListener('click', this.scrollTop);
+    document.addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const scrollTopButton = document.querySelector('.beej-back-to-top-button') as HTMLElement;
+    const footer = document.querySelector('.beej-footer') as HTMLElement;
+    let footerPosition = footer.getBoundingClientRect();
+
+    if (footerPosition.top < window.innerHeight && footerPosition.bottom >= 0) {
+      scrollTopButton.style.display = "block";
+    } else {
+      scrollTopButton.style.display = "none";
+    }
+  }
+
+  scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+}
+
+const backToTop = new BackToTop();
