32a076ddfd193c66da97cc92e8b89a21095cc122
BJ Viehl <viehl.design@gmail.com>
2021-09-07 22:35:33 -0600

    todo list wip

--- ui.frontend/src/main/webpack/components/content/todolist/ts/_todolist.ts
+++ ui.frontend/src/main/webpack/components/content/todolist/ts/_todolist.ts
@@ -3,151 +3,151 @@
 // } from '@dependencies/documentReady';
 // import { deviceCheck } from '@dependencies/deviceCheck';
 
-class TodoList {
-  constructor() {
-    this.init();
-  }
+// class TodoList {
+//   constructor() {
+//     this.init();
+//   }
 
-  init = () => {
-    document.addEventListener('DOMContentLoaded', this.getTasks);
-    document.querySelector('#todo-form').addEventListener('submit', this.addTask);
-    document.querySelector('#todo-list').addEventListener('click', this.removeTask);
-    document.querySelector('.todo-clear').addEventListener('click', this.clearTasks);
-    document.querySelector('#filter').addEventListener('keyup', this.filterTasks);
-  }
+//   init = () => {
+//     document.addEventListener('DOMContentLoaded', this.getTasks);
+//     document.querySelector('#todo-form').addEventListener('submit', this.addTask);
+//     document.querySelector('#todo-list').addEventListener('click', this.removeTask);
+//     document.querySelector('.todo-clear').addEventListener('click', this.clearTasks);
+//     document.querySelector('#filter').addEventListener('keyup', this.filterTasks);
+//   }
 
-  getTasks() {
-    let tasks;
+//   getTasks() {
+//     let tasks;
   
-    if (localStorage.getItem('tasks') === null) {
-      tasks = [];
-    } else {
-      tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
+//     if (localStorage.getItem('tasks') === null) {
+//       tasks = [];
+//     } else {
+//       tasks = JSON.parse(localStorage.getItem('tasks'));
+//     }
 
-    const list: HTMLElement = document.querySelector('#todo-list');
+//     const list: HTMLElement = document.querySelector('#todo-list');
 
-    tasks.forEach((task) => {
-      const li: HTMLElement = document.createElement('li');
-      const link: HTMLElement = document.createElement('a');
+//     tasks.forEach((task) => {
+//       const li: HTMLElement = document.createElement('li');
+//       const link: HTMLElement = document.createElement('a');
 
-      li.className = 'beej-todo-list__list-item animate__animated animate__backOutLeft animate__bounceInUp ';
-      li.appendChild(document.createTextNode(task));
-      link.className = 'beej-todo-list__list-item-delete';
-      link.innerHTML = '<i class="fa fa-times"></i>';
+//       li.className = 'beej-todo-list__list-item animate__animated animate__bounceInUp ';
+//       li.appendChild(document.createTextNode(task));
+//       link.className = 'beej-todo-list__list-item-delete';
+//       link.innerHTML = '<i class="fa fa-times"></i>';
 
-      li.appendChild(link);
-      list.appendChild(li);
-    });
-  }
+//       li.appendChild(link);
+//       list.appendChild(li);
+//     });
+//   }
 
-  addTask = (e) => {
-    const input: HTMLInputElement = document.querySelector('#todo-input');
-    const list: HTMLElement = document.querySelector('#todo-list');
+//   addTask = (e) => {
+//     const input: HTMLInputElement = document.querySelector('#todo-input');
+//     const list: HTMLElement = document.querySelector('#todo-list');
 
-    if (input.value === '') {
-      alert('Add a task please');
-    } else {
-      const li: HTMLElement = document.createElement('li');
-      const link: HTMLElement = document.createElement('a');
+//     if (input.value === '') {
+//       alert('Add a task please');
+//     } else {
+//       const li: HTMLElement = document.createElement('li');
+//       const link: HTMLElement = document.createElement('a');
 
-      li.className = 'beej-todo-list__list-item animate__animated animate__backOutLeft animate__bounceInUp ';
-      li.appendChild(document.createTextNode(input.value));
-      link.className = 'beej-todo-list__list-item-delete';
-      link.innerHTML = '<i class="fa fa-times"></i>';
+//       li.className = 'beej-todo-list__list-item animate__animated animate__backOutLeft animate__bounceInUp ';
+//       li.appendChild(document.createTextNode(input.value));
+//       link.className = 'beej-todo-list__list-item-delete';
+//       link.innerHTML = '<i class="fa fa-times"></i>';
 
-      li.appendChild(link);
-      list.appendChild(li);
+//       li.appendChild(link);
+//       list.appendChild(li);
 
-      this.addTaskLS(input.value);
-      this.addNoticeSS(input.value);
-    }
+//       this.addTaskLS(input.value);
+//       this.addNoticeSS(input.value);
+//     }
 
-    input.value = '';
+//     input.value = '';
   
-    e.preventDefault();
-  }
+//     e.preventDefault();
+//   }
 
-  addTaskLS = (task) => {
-    let tasks;
+//   addTaskLS = (task) => {
+//     let tasks;
   
-    if (localStorage.getItem('tasks') === null) {
-      tasks = [];
-    } else {
-      tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
+//     if (localStorage.getItem('tasks') === null) {
+//       tasks = [];
+//     } else {
+//       tasks = JSON.parse(localStorage.getItem('tasks'));
+//     }
 
-    tasks.push(task);
+//     tasks.push(task);
 
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
+//     localStorage.setItem('tasks', JSON.stringify(tasks));
+//   }
 
-  addNoticeSS = (task) => {
-    let notices;
+//   addNoticeSS = (task) => {
+//     let notices;
 
-    if (localStorage.getItem('notices') === null || localStorage.getItem('notices') === '') {
-      notices = [];
-    } else {
-      notices = JSON.parse(localStorage.getItem('notices'));
-    }
+//     if (localStorage.getItem('notices') === null || localStorage.getItem('notices') === '') {
+//       notices = [];
+//     } else {
+//       notices = JSON.parse(localStorage.getItem('notices'));
+//     }
 
-    let notice = `User added '${task}' to the todo list`;
+//     let notice = `User added '${task}' to the todo list`;
 
-    notices.push(notice);
+//     notices.push(notice);
   
-    localStorage.setItem('notices', JSON.stringify(notices));
-  }
+//     localStorage.setItem('notices', JSON.stringify(notices));
+//   }
 
-  removeTask = (e) => {
-    if (e.target.parentElement.classList.contains('beej-todo-list__list-item-delete')) {
-      e.target.parentElement.parentElement.remove();
-      this.removeTaskLS(e.target.parentElement.parentElement);
-    }
+//   removeTask = (e) => {
+//     if (e.target.parentElement.classList.contains('beej-todo-list__list-item-delete')) {
+//       e.target.parentElement.parentElement.remove();
+//       this.removeTaskLS(e.target.parentElement.parentElement);
+//     }
   
-    e.preventDefault();
-  }
+//     e.preventDefault();
+//   }
 
-  removeTaskLS = (taskItem) => {
-    let tasks;
+//   removeTaskLS = (taskItem) => {
+//     let tasks;
   
-    if (localStorage.getItem('tasks') === null) {
-      tasks = [];
-    } else {
-      tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
+//     if (localStorage.getItem('tasks') === null) {
+//       tasks = [];
+//     } else {
+//       tasks = JSON.parse(localStorage.getItem('tasks'));
+//     }
   
-    tasks.forEach((task, index) => {
-      if (taskItem.textContent === task) {
-        tasks.splice(index, 1);
-      }
-    });
+//     tasks.forEach((task, index) => {
+//       if (taskItem.textContent === task) {
+//         tasks.splice(index, 1);
+//       }
+//     });
   
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
+//     localStorage.setItem('tasks', JSON.stringify(tasks));
+//   }
 
-  filterTasks(e) {
-    const text = e.target.value.toLowerCase();
+//   filterTasks(e) {
+//     const text = e.target.value.toLowerCase();
   
-    document.querySelectorAll('.beej-todo-list__list-item').forEach((task) => {
-      let item = task.firstChild.textContent as String;
-      let taskI = task as HTMLElement;
+//     document.querySelectorAll('.beej-todo-list__list-item').forEach((task) => {
+//       let item = task.firstChild.textContent as String;
+//       let taskI = task as HTMLElement;
   
-      if (item.toLowerCase().indexOf(text) !== -1) {
-        taskI.style.display = 'flex';
-      } else {
-        taskI.style.display = 'none';
-      }
-    });
-  }
+//       if (item.toLowerCase().indexOf(text) !== -1) {
+//         taskI.style.display = 'flex';
+//       } else {
+//         taskI.style.display = 'none';
+//       }
+//     });
+//   }
 
-  clearTasks = () => {
-    const list: HTMLElement = document.querySelector('#todo-list');
+//   clearTasks = () => {
+//     const list: HTMLElement = document.querySelector('#todo-list');
 
-    while (list.firstChild) {
-      list.removeChild(list.firstChild);
-    }
-    localStorage.clear();
-  }
-}
+//     while (list.firstChild) {
+//       list.removeChild(list.firstChild);
+//     }
+//     localStorage.clear();
+//   }
+// }
 
-const todoList = new TodoList();
+// const todoList = new TodoList();
--- ui.frontend/src/main/webpack/components/content/todolistaudit/ts/_todolistaudit.ts
+++ ui.frontend/src/main/webpack/components/content/todolistaudit/ts/_todolistaudit.ts
@@ -1,40 +1,40 @@
-class TodoListAudit {
-  constructor() {
-    this.init();
-  }
+// class TodoListAudit {
+//   constructor() {
+//     this.init();
+//   }
 
-  init = () => {
-    document.addEventListener('DOMContentLoaded', this.getNotices);
-    window.addEventListener('storage', this.showNotices)
-  }
+//   init = () => {
+//     document.addEventListener('DOMContentLoaded', this.getNotices);
+//     window.addEventListener('storage', this.showNotices)
+//   }
 
-  getNotices = () => {
-    let notices;
+//   getNotices = () => {
+//     let notices;
   
-    if (localStorage.getItem('notices') === null) {
-      notices = [];
-    } else {
-      notices = [JSON.parse(localStorage.getItem('notices'))];
-    }
+//     if (localStorage.getItem('notices') === null) {
+//       notices = [];
+//     } else {
+//       notices = [JSON.parse(localStorage.getItem('notices'))];
+//     }
 
-    const output: HTMLElement = document.querySelector('#list-output');
+//     const output: HTMLElement = document.querySelector('#list-output');
 
-    notices.forEach((notice) => {
-      const noticeItem: HTMLElement = document.createElement('li');
+//     notices.forEach((notice) => {
+//       const noticeItem: HTMLElement = document.createElement('li');
 
-      noticeItem.className = 'beej-todo-list-audit__list-item animate__animated animate__fadeInUp ';
-      noticeItem.appendChild(document.createTextNode(notice));
+//       noticeItem.className = 'beej-todo-list-audit__list-item animate__animated animate__fadeInUp ';
+//       noticeItem.appendChild(document.createTextNode(notice));
 
-      output.appendChild(noticeItem);
-    });
+//       output.appendChild(noticeItem);
+//     });
 
-    console.log('on page load');
-  }
+//     console.log('on page load');
+//   }
 
-  showNotices = (e) => {
+//   showNotices = (e) => {
 
-    console.log('on click');
-  }
-}
+//     console.log('on click');
+//   }
+// }
 
-const todoListAudit = new TodoListAudit();
+// const todoListAudit = new TodoListAudit();
